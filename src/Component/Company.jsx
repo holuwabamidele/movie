67550ba0b935a6aab30267b6749fd314707ef260
holuwabamidele <yemo4real@ymail.com>
2024-07-23 15:01:15 +0100

    movie

--- src/Component/Company.jsx
+++ src/Component/Company.jsx
@@ -58,7 +58,7 @@ const Container = styled.div`
   grid-template-columns: repeat(5, minmax(0, 1fr));
 
   @media (max-width: 500px) {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
+    grid-template-columns: repeat(1, minmax(0, 1fr));
   }
 `;
 const Card = styled.div`
